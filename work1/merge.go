d5c584c888eeb9aa68d733686314da0c327c30f0
Edward09292018 <git@github.com:Edward09292018/repo.git>
2025-07-06 23:33:18 +0800

    Changes

--- work1/merge.go
+++ work1/merge.go
@@ -1,31 +1,37 @@
+package main
+
+import (
+	"sort"
+)
+
 func merge(intervals [][]int) [][]int {
-    if len(intervals) == 0 {
-        return [][]int{}
-    }
+	if len(intervals) == 0 {
+		return [][]int{}
+	}
 
-    // 按照起始位置排序
-    sort.Slice(intervals, func(i, j int) bool {
-        return intervals[i][0] < intervals[j][0]
-    })
+	// 按照起始位置排序
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
 
-    var merged [][]int
-    for _, interval := range intervals {
-        // 如果 merged 为空或者当前区间的起始位置大于 merged 中最后一个区间的结束位置，则直接添加
-        if len(merged) == 0 || interval[0] > merged[len(merged)-1][1] {
-            merged = append(merged, interval)
-        } else {
-            // 否则，合并区间
-            merged[len(merged)-1][1] = max(interval[1], merged[len(merged)-1][1])
-        }
-    }
+	var merged [][]int
+	for _, interval := range intervals {
+		// 如果 merged 为空或者当前区间的起始位置大于 merged 中最后一个区间的结束位置，则直接添加
+		if len(merged) == 0 || interval[0] > merged[len(merged)-1][1] {
+			merged = append(merged, interval)
+		} else {
+			// 否则，合并区间
+			merged[len(merged)-1][1] = max(interval[1], merged[len(merged)-1][1])
+		}
+	}
 
-    return merged
+	return merged
 }
 
 // 辅助函数用于取最大值
 func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
 }
--- work1/removeDuplicates.go
+++ work1/removeDuplicates.go
@@ -1,18 +1,20 @@
+package main
+
 func removeDuplicates(nums []int) int {
-    if len(nums) == 0 {
-        return 0
-    }
+	if len(nums) == 0 {
+		return 0
+	}
 
-    // 指针 i 记录唯一元素的位置
-    k := 1
+	// 指针 i 记录唯一元素的位置
+	k := 1
 
-    // 遍历数组，如果当前元素与前一个不同，则放入 nums[k]
-    for i := 1; i < len(nums); i++ {
-        if nums[i] != nums[k-1] {
-            nums[k] = nums[i]
-            k++
-        }
-    }
+	// 遍历数组，如果当前元素与前一个不同，则放入 nums[k]
+	for i := 1; i < len(nums); i++ {
+		if nums[i] != nums[k-1] {
+			nums[k] = nums[i]
+			k++
+		}
+	}
 
-    return k
+	return k
 }
